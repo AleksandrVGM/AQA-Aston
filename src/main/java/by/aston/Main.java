ebde534cc9c0d1c2223ad04359eab5d428111ca6
Aleksandr <aleksandr.masterkevich@gmail.com>
2024-12-09 18:39:05 +0300

    fixed task1-task3

--- src/main/java/by/aston/Main.java
+++ src/main/java/by/aston/Main.java
@@ -1,7 +1,29 @@
 package by.aston;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //task2
+        Employee[] employees = new Employee[5];
+        employees[0] = new Employee("Bill Gates", "Retiree", "BillGates@gmail.com", "+375 84 5932659", 0, 69);
+        employees[1] = new Employee("Elon Musk", "Twitter owner", "ElonMusk@gmail.com", "+375 42 5649281", 99, 53);
+        employees[2] = new Employee("Larry Page", "CEO", "LarryPage@gmail.com", "+375 14 5843987", 999, 51);
+        employees[3] = new Employee("Sergey Brin", "Co-founder Alphabet Inc.", "SergeyBrin.com", "+375 25 5894613", 9999, 51);
+        employees[4] = new Employee("Larry Ellison", "CTO", "LarryEllison@gmail.com", "+375 28 3216548", 99999, 80);
+
+        Park.Attraction attraction1 = new Park.Attraction("Ferris wheel", "10:00-22:00", 10);
+        Park.Attraction attraction2 = new Park.Attraction("Rollercoaster", "10:00-22:00", 15);
+        Park.Attraction attraction3 = new Park.Attraction("Bumper cars", "10:00-22:00", 20);
+        Park.Attraction attraction4 = new Park.Attraction("Funhouse", "10:00-22:00", 25);
+        Park.Attraction attraction5 = new Park.Attraction("Pendulum ride", "10:00-22:00", 18);
+        Park park = new Park(new Park.Attraction[]{attraction1, attraction2, attraction3});
+        park.addAttraction(attraction4);
+        park.addAttraction(attraction5);
+
+        System.out.println(park);
+        park.deleteAttraction("Funhouse");
+
+        System.out.println(park);
     }
 }
--- src/main/java/by/aston/Park.java
+++ src/main/java/by/aston/Park.java
@@ -1,16 +1,56 @@
 package by.aston;
 
+import java.util.Arrays;
+
 public class Park {
     private Attraction[] attractions;
+    private int attractionCount = 0;
+
+    public Park() {
+        attractions = new Attraction[3];
+    }
 
     public Park(Attraction[] attractions) {
         this.attractions = attractions;
+        attractionCount = attractions.length;
+    }
+
+    public void addAttraction(Attraction attraction) {
+        if (attractions.length <= attractionCount) {
+            attractions = Arrays.copyOf(attractions, attractions.length * 2);
+        }
+        attractions[attractionCount] = attraction;
+        attractionCount++;
+    }
+
+    public void deleteAttraction(String attractionName) {
+        int index = 0;
+        for (int i = 0; i < attractionCount; i++) {
+            if (attractions[i].name.compareTo(attractionName) == 0) {
+                index = i;
+                break;
+            }
+        }
+        for (int i = index; i < attractionCount; i++) {
+            attractions[i] = i == attractions.length - 1 ? null : attractions[i + 1];
+        }
+        attractionCount--;
+    }
+
+    @Override
+    public String toString() {
+        return "Park{" + "attractions=" + Arrays.toString(attractions) + '}';
     }
 
     static class Attraction {
         private String name;
         private String workTime;
         private int cost;
+
+        @Override
+        public String toString() {
+            return "Attraction{" + "name='" + name + '\'' + ", workTime='" + workTime + '\'' + ", cost=" + cost + '}';
+        }
 
         public Attraction(String name, String workTime, int cost) {
             this.name = name;
