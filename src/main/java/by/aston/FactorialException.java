b00ee62d08622efa972d5ddd278f0adba751a625
Aleksandr <aleksandr.masterkevich@gmail.com>
2024-12-23 21:56:52 +0300

    task14 - jUnit

--- src/main/java/by/aston/FactorialException.java
+++ src/main/java/by/aston/FactorialException.java
@@ -1,10 +1,9 @@
 package by.aston;
 
-public class FactorialException extends IllegalArgumentException {
+public class FactorialException extends RuntimeException {
 
     public static final String NEGATIVE_NUMBER = "The number is negative. The number must be greater than zero.";
     public static final String TOO_BIG_NUMBER = "Can't calculate factorial for this number = %d.";
-
 
     public FactorialException() {
     }
@@ -20,8 +19,8 @@ public class FactorialException extends IllegalArgumentException {
     public FactorialException(Throwable cause) {
         super(cause);
     }
-/*
+
     public FactorialException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
         super(message, cause, enableSuppression, writableStackTrace);
-    }*/
+    }
 }
