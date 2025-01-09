a41e87c297a57ef948285ea82ee83ea0b53d9f6a
Aleksandr <aleksandr.masterkevich@gmail.com>
2025-01-09 23:15:05 +0300

    Task15

--- src/test/java/by/aston/MTSTest.java
+++ src/test/java/by/aston/MTSTest.java
@@ -13,7 +13,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 class MTSTest {
-    private static WebDriver driver;
     private static final String NAME_BUTTON_CANCEL = "Отклонить";
     private static final String NAME_BLOCK = "Онлайн пополнение\nбез комиссии";
     private static final String LOGO_VISA = "Visa";
@@ -22,7 +21,9 @@ class MTSTest {
     private static final String LOGO_MASTERCARD_SECURE_CODE = "MasterCard Secure Code";
     private static final String LOGO_BELKART = "Белкарт";
     private static final String PAGE_TITLE = "Порядок оплаты и безопасность интернет платежей";
-
+    private static final String PHONE_NUMBER = "297777777";
+    private static final String PAY_DESCRIPTION_TEXT = "Оплата: Услуги связи Номер:375%s";
+    private static WebDriver driver;
 
     @BeforeAll
     static void beforeAll() {
@@ -31,26 +32,25 @@ class MTSTest {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
 
+    @AfterAll
+    static void afterAll() {
+        driver.quit();
+    }
+
     @BeforeEach
-    void beforeEach(){
+    void beforeEach() {
         driver.get("https://mts.by");
         onCanceledCookies();
     }
 
-    public void onCanceledCookies(){
+    public void onCanceledCookies() {
         try {
             new WebDriverWait(driver, Duration.ofSeconds(3)).until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.className("cookie")));
             driver.findElement(By.className("cookie")).findElements(By.tagName("button")).stream()
                     .filter(button -> button.getText().equals(NAME_BUTTON_CANCEL)).findFirst().get().click();
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             //
         }
-    }
-
-    @AfterAll
-    static void afterAll() {
-        driver.quit();
     }
 
     @Test
@@ -60,37 +60,9 @@ class MTSTest {
         Assertions.assertEquals(NAME_BLOCK, webElement.getText());
     }
 
-    @Test
-    public void logoVisaTest() {
-        String visa = "Visa";
-        Assertions.assertEquals(true, getLogoNames().contains(LOGO_VISA));
-    }
 
     @Test
-    public void logoVerifiedByVisaTest() {
-        String visa = "Verified By Visa";
-        Assertions.assertEquals(true, getLogoNames().contains(visa));
-    }
-
-    @Test
-    public void logoMasterCardTest() {
-        String visa = "MasterCard";
-        Assertions.assertEquals(true, getLogoNames().contains(visa));
-    }
-
-    @Test
-    public void logoMasterCardSecureCodeTest() {
-        String visa = "MasterCard Secure Code";
-        Assertions.assertEquals(true, getLogoNames().contains(visa));
-    }
-
-    @Test
-    public void logoBelkartTest() {
-        String visa = "Белкарт";
-        Assertions.assertEquals(true, getLogoNames().contains(visa));
-    }
-    @Test
-    public void logTest(){
+    public void logoTest() {
         Assertions.assertEquals(true, getLogoNames().contains(LOGO_VISA));
         Assertions.assertEquals(true, getLogoNames().contains(LOGO_VERIFIED_BY_VISA));
         Assertions.assertEquals(true, getLogoNames().contains(LOGO_MASTERCARD));
@@ -99,18 +71,17 @@ class MTSTest {
         Assertions.assertEquals(5, getLogoNames().size());
 
         List<String> logoNames = getLogoNames();
-        logoNames.removeAll(List.of(LOGO_VISA,LOGO_VERIFIED_BY_VISA,LOGO_MASTERCARD,LOGO_MASTERCARD_SECURE_CODE,LOGO_BELKART));
+        logoNames.removeAll(List.of(LOGO_VISA, LOGO_VERIFIED_BY_VISA, LOGO_MASTERCARD, LOGO_MASTERCARD_SECURE_CODE, LOGO_BELKART));
         Assertions.assertEquals(0, logoNames.size());
 
-        for (WebElement logo: getLogos()) {
+        for (WebElement logo : getLogos()) {
             Assertions.assertEquals(true, logo.isDisplayed(), logo.getAccessibleName() + " is not displayed");
         }
     }
 
-    public List<WebElement> getLogos(){
+    public List<WebElement> getLogos() {
         return driver.findElement(By.className("pay__partners")).findElements(By.cssSelector("img"));
     }
-
 
     public List<String> getLogoNames() {
         return getLogos().stream()
@@ -127,21 +98,24 @@ class MTSTest {
         link.click();
         Assertions.assertEquals(url, driver.getCurrentUrl());
         Assertions.assertEquals(PAGE_TITLE, driver.getTitle());
-
-        List<String> list = driver.findElements(By.tagName("h3")).stream().map(webElement -> webElement.getText()).collect(Collectors.toList());
-
-        Assertions.assertEquals(true, list.containsAll(List.of("Оплата банковской картой", "Информация о безопасности Интернет-платежей", "Условия возврата денежных средств")));
+        List<String> infoOnPage = driver.findElements(By.tagName("h3")).stream().map(WebElement::getText).collect(Collectors.toList());
+        Assertions.assertEquals(true, infoOnPage.containsAll(List.of("Оплата банковской картой", "Информация о безопасности Интернет-платежей", "Условия возврата денежных средств")));
     }
 
     @Test
-    public void paymentTest(){
+    public void paymentTest() {
         WebElement select_now = driver.findElement(By.className("select__header")).findElement(By.className("select__now"));
         WebElement phone = driver.findElement(By.xpath("//*[@id=\"connection-phone\"]"));
-        phone.sendKeys("297777777");
+        phone.sendKeys(PHONE_NUMBER);
         WebElement money = driver.findElement(By.xpath("//*[@id=\"connection-sum\"]"));
         money.sendKeys("11");
         WebElement button = driver.findElement(By.xpath("//*[@id=\"pay-connection\"]")).findElement(By.className("button"));
         button.click();
-        System.out.println("");
+        new WebDriverWait(driver, Duration.ofSeconds(5)).until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(By.className("bepaid-iframe")));
+        new WebDriverWait(driver, Duration.ofSeconds(5)).until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.className("pay-description__text")));
+        String payDescriptionText = driver.findElement(By.className("pay-description__text")).getText();
+        Assertions.assertEquals(String.format(PAY_DESCRIPTION_TEXT, PHONE_NUMBER), payDescriptionText);
     }
+
+
 }
